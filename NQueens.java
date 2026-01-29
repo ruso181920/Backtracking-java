25beb62501ad2de9a2ad7f861364282caf7e51be
Raushan Kumar <rraushan4590@gmail.com>
2026-01-29 08:35:29 +0530

    The NQueen Problem

---
+++ NQueens.java
@@ -0,0 +1,63 @@
+public class NQueens {
+    public static boolean isSafe(char board[][],int row,int col) {
+        
+        //vertical row checkup
+        for (int i = row-1; i >= 0; i--) {
+            if(board[i][col]=='Q') {
+                return false;
+            }
+        }
+         //diagonal left checkup
+        for (int i = row-1,j=col-1; i >= 0 && j>=0; i--,j--) {
+            if(board[i][j]=='Q') {
+                return false;
+            }
+        }
+        //diagonal right checkup
+       for (int i = row-1,j=col+1; i >= 0 && j<board.length; i--,j++) {
+            if(board[i][j]=='Q') {
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public static void nQueens(char board[][],int row) {
+        //base case
+        if(row==board.length) {
+            printBoard(board);
+            return;
+        }
+        // column loop
+       for (int j = 0; j < board.length; j++) {
+        if(isSafe(board,row,j)) {
+         board[row][j]='Q';
+        //function call
+        nQueens(board, row+1);
+        //backTracking
+        board[row][j]='X';
+    }
+}
+}
+  public static void printBoard(char board[][]) {
+    System.out.println("--------chess board----");
+      for (int i = 0; i < board.length; i++) {
+          for (int j = 0; j < board.length; j++) {
+              System.out.print(board[i][j]);
+          }
+          System.out.println();
+      }
+  }
+    public static void main (String args[]) {
+        int n=5;
+        char board [][]=new char [n][n];
+        ///intialize
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j]='X';
+
+            }
+        }
+        nQueens(board,0);
+    }
+}
---
+++ Permutattions.java
@@ -0,0 +1,21 @@
+public class Permutattions {
+    public static void findPermutation(String str,String ans) {
+        //base case 
+        if(str.length()==0) {
+            System.out.println(ans);
+            return;
+        }
+        //recursion
+        for (int i = 0; i < str.length(); i++) {
+            char curr=str.charAt(i);
+
+            //removing 
+           String newStr=str.substring(0,i) +str.substring(i+1);
+           findPermutation(newStr, ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        findPermutation(str,"");
+    }
+}
